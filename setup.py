298b29b8b1b398c4f360ac17ea5318f7d4e0c6f7
Ivan Velichko <mail4ostrovski@gmail.com>
2016-05-15 13:50:31 +0300

    Initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pgreen',
+      version='0.1',
+      description='Handy Greenlet profiler',
+      url='http://github.com/Ostrovski/pgreen',
+      author='Ivan Velichko',
+      author_email='iximiuz@gmail.com',
+      license='MIT',
+      packages=['pgreen'],
+      zip_safe=False)
